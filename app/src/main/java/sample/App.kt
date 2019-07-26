fd87644c6b671e2dc92a0a3436400a8492c50a2d
Marcin Moskala <marcinmoskala@gmail.com>
2019-07-26 07:09:35 +0200

    Add App with Koin builders

---
+++ app/src/main/java/sample/App.kt
@@ -0,0 +1,18 @@
+package sample
+
+import android.app.Application
+import org.koin.android.ext.android.startKoin
+import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val androidModule = module {
+            single<Timer> { AndroidTimer() }
+            single<Speaker> { AndroidSpeaker(get()) }
+            viewModel { WorkoutViewModel(get(), get()) }
+        }
+        startKoin(this, listOf(androidModule))
+    }
+}
